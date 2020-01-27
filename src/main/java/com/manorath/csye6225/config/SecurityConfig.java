5b77733607a945e5f8f153b0a73fe57b66f2c540
Manorath Bajaj <bajaj.m@northeastern.edu>
2020-01-26 20:17:01 -0500

    stateless addition

--- src/main/java/com/manorath/csye6225/config/SecurityConfig.java
+++ src/main/java/com/manorath/csye6225/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -29,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.sessionManagement().and().csrf().disable().cors().and().httpBasic()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable().cors().and().httpBasic()
                 .and().authorizeRequests()
                 .antMatchers("v1/user/**","/v1/bill/**","v1/bills")
                 .authenticated();
